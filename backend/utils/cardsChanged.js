19516285b333580c3e3ea9d177b387e7d132c89c
MegaLoogin <veh23sab@yandex.ru>
2025-02-18 21:52:19 +0300

    Update cardsChanged.js

--- backend/utils/cardsChanged.js
+++ backend/utils/cardsChanged.js
@@ -1,6 +1,9 @@
 const { onCardsChange, moveCard } = require("./trello");
 
-const { DESIGN_BOARD, DESIGN_LIST_WORK, DESIGN_LIST_DONE, DESIGN_CHAN_ID, BIZDEV_BOARD, BIZDEV_LIST_WORK, BIZDEV_LIST_DONE, BIZDEV_CHAN_ID, FB_BOARD, FB_LIST_NEW, FB_LIST_BUYERS, FB_TEMPLATE_NAME } = process.env;
+const { DESIGN_BOARD, DESIGN_LIST_WORK, DESIGN_LIST_DONE, DESIGN_CHAN_ID, 
+    BIZDEV_BOARD, BIZDEV_PUSH_BOARD, BIZDEV_LIST_WORK, BIZDEV_LIST_DONE, BIZDEV_CHAN_ID, 
+    FB_BOARD, FB_LIST_NEW, FB_LIST_BUYERS, FB_TEMPLATE_NAME, FB_LIST_TROUBLE,
+    PUSH_BOARD, PUSH_LIST_NEW, PUSH_LIST_BUYERS, PUSH_TEMPLATE_NAME, PUSH_LIST_TROUBLE } = process.env;
 
 module.exports = function (client){
     onCardsChange(DESIGN_BOARD, DESIGN_LIST_WORK, async dif => {
@@ -68,6 +71,56 @@ module.exports = function (client){
             });
         });
     });
+
+    FB_LIST_BUYERS.split(',').forEach(buyerName => {
+        onCardsChange(buyerName + " " + FB_TEMPLATE_NAME, FB_LIST_TROUBLE, async dif => {
+            dif.forEach(async card => {
+                await moveCard(BIZDEV_BOARD, FB_LIST_TROUBLE, card.id);
+            });
+        });
+    });
+    
+
+
+    onCardsChange(BIZDEV_PUSH_BOARD, BIZDEV_LIST_DONE, async dif => {
+        dif.forEach(async card => {
+            const match = card.desc.match(/@([A-Za-z0-9]+)/);
+
+            await moveCard(PUSH_BOARD, PUSH_LIST_NEW, card.id);
+
+            if(match){
+                await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] <@${match[1]}> добавлен новый оффер (${card.name})`);
+            }else{
+                await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] Добавлен новый оффер (${card.name})`);
+            }
+        });
+    });
+
+    PUSH_LIST_BUYERS.split(',').forEach(buyerName => {
+        onCardsChange(PUSH_BOARD, buyerName, async dif => {
+            dif.forEach(async card => {
+                const match = card.desc.match(/@([A-Za-z0-9]+)/);
+
+                await moveCard(buyerName + " " + PUSH_TEMPLATE_NAME, PUSH_LIST_NEW, card.id);
+    
+                if(match){
+                    await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] <@${match[1]}> добавлен новый оффер. Проверьте свою доску`);
+                }else{
+                    await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] Добавлен новый оффер. Проверьте свою доску`);
+                }
+            });
+        });
+    });
+
+    PUSH_LIST_BUYERS.split(',').forEach(buyerName => {
+        onCardsChange(buyerName + " " + PUSH_TEMPLATE_NAME, PUSH_LIST_TROUBLE, async dif => {
+            dif.forEach(async card => {
+                await moveCard(BIZDEV_BOARD, PUSH_LIST_TROUBLE, card.id);
+            });
+        });
+    });
+
+
 
     onCardsChange(TECH_BOARD, TECH_LIST_WORK, async dif => {
         dif.forEach(async card => {
