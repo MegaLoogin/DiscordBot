824f4e76c95b2b8670b42ab3bc14d5fbf0f8e816
MegaLoogin <veh23sab@yandex.ru>
2025-02-20 21:28:42 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,8 +29,8 @@ const client = new Client({intents:
 	[GatewayIntentBits.Guilds,
 	GatewayIntentBits.GuildMessages,
 	GatewayIntentBits.MessageContent,
-	// GatewayIntentBits.GuildVoiceStates,
-    // GatewayIntentBits.GuildPresences,
+	GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
     GatewayIntentBits.GuildMembers]});
 const gapi = new GoogleAPI();
 
