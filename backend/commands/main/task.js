671c0d58efae77a40ef346d0ebad99655d00e65d
MegaLoogin <veh23sab@yandex.ru>
2025-02-20 23:18:15 +0300

    Update task.js

--- backend/commands/main/task.js
+++ backend/commands/main/task.js
@@ -33,19 +33,23 @@ module.exports = {
         .addStringOption(opt => opt.setName('дедлайн').setDescription('Дата выполнения').setRequired(true))
         .addStringOption(opt => opt.setName('дизайнер').setDescription('Дизайнер').setRequired(true).addChoices(DESIGNERS_IDS.split(',').map(v => {return {name: v.split('|')[1], value: v} }))),
 	async execute(int) {
+        console.log('Получил команду');
         await int.deferReply();
         const date = validateDate(int.options.getString('дедлайн'));
         const [designerId, designerName] = int.options.getString('дизайнер').split('|');
-
+        console.log('1')
         if(!date) {
             await int.reply({content: 'Неверный формат даты (дд.мм.гггг или дд.мм). Пример: 02.03 или 02.03.2025', flags: MessageFlags.Ephemeral});
             return;
         }
+        console.log('2')
 
         let dateString = convertDate(date.year ? int.options.getString('дедлайн') : ( ( ( new Date(`${new Date().getFullYear()}-${date.month}-${date.day}`) - new Date(new Date().toDateString()) ) < 0)? `${date.day}.${date.month}.${new Date().getFullYear() + 1}` : `${date.day}.${date.month}.${new Date().getFullYear()}` ))
-        
+        console.log(3)
         await createNewCard(process.env.DESIGN_BOARD, process.env.DESIGN_LIST_REQUESTS, `${int.options.getString('название')} (${designerName})`, int.options.getString('описание') + `\nfrom [@${int.user.displayName} <@${int.user.id}> ${int.channelId}]\nto <@${designerId}>`, "bottom", dateString, int.options.getString('приоритет'));
+        console.log(4)
         const queueData = await getQueue(process.env.DESIGN_BOARD, process.env.DESIGN_LIST_REQUESTS);
+        console.log(5)
         await int.editReply(`Новая задача "${int.options.getString('название')}". Очередь: ${queueData.count} задач`);
 	},
 };
