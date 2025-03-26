d8587daf5c697085c129d22ea96904d919863cc7
MegaLoogin <veh23sab@yandex.ru>
2025-03-26 13:35:52 +0300

    Update cardsChanged.js

--- backend/utils/cardsChanged.js
+++ backend/utils/cardsChanged.js
@@ -1,7 +1,7 @@
 const { onCardsChange, moveCard } = require("./trello");
 
 const { DESIGN_BOARD, DESIGN_LIST_WORK, DESIGN_LIST_DONE, DESIGN_CHAN_ID, 
-    BIZDEV_BOARD, BIZDEV_PUSH_BOARD, BIZDEV_LIST_WORK, BIZDEV_LIST_DONE, BIZDEV_CHAN_ID, BIZDEV_LIST_TEST_DONE,
+    BIZDEV_BOARD, BIZDEV_PUSH_BOARD, BIZDEV_LIST_WORK, BIZDEV_LIST_DONE, BIZDEV_CHAN_ID, BIZDEV_PUSH_CHAN_ID, BIZDEV_LIST_TEST_DONE,
     FB_BOARD, FB_LIST_NEW, FB_LIST_BUYERS, FB_TEMPLATE_NAME, FB_LIST_TROUBLE,
     PUSH_BOARD, PUSH_LIST_NEW, PUSH_LIST_BUYERS, PUSH_TEMPLATE_NAME, PUSH_LIST_TROUBLE,
     TECH_BOARD, TECH_LIST_WORK, TECH_LIST_DONE, TECH_CHAN_ID } = process.env;
@@ -107,9 +107,9 @@ module.exports = function (client){
             const match = card.desc.match(/@([A-Za-z0-9]+)/);
 
             if(match){
-                await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] <@${match[1]}> Ваш запрос (${card.name}) на этапе подключения`);
+                await client.channels.cache.get(BIZDEV_PUSH_CHAN_ID).send(`[Trello] <@${match[1]}> Ваш запрос (${card.name}) на этапе подключения`);
             }else{
-                await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] Ваш запрос (${card.name}) на этапе подключения`);
+                await client.channels.cache.get(BIZDEV_PUSH_CHAN_ID).send(`[Trello] Ваш запрос (${card.name}) на этапе подключения`);
             }
         });
     });
@@ -121,9 +121,9 @@ module.exports = function (client){
             await moveCard(PUSH_BOARD, PUSH_LIST_NEW, card.id);
 
             if(match){
-                await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] <@${match[1]}> добавлен новый оффер (${card.name})`);
+                await client.channels.cache.get(BIZDEV_PUSH_CHAN_ID).send(`[Trello] <@${match[1]}> добавлен новый оффер (${card.name})`);
             }else{
-                await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] Добавлен новый оффер (${card.name})`);
+                await client.channels.cache.get(BIZDEV_PUSH_CHAN_ID).send(`[Trello] Добавлен новый оффер (${card.name})`);
             }
         });
     });
@@ -136,9 +136,9 @@ module.exports = function (client){
                 await moveCard(buyerName + " " + PUSH_TEMPLATE_NAME, PUSH_LIST_NEW, card.id);
     
                 if(match){
-                    await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] <@${match[1]}> добавлен новый оффер. Проверьте свою доску`);
+                    await client.channels.cache.get(BIZDEV_PUSH_CHAN_ID).send(`[Trello] <@${match[1]}> добавлен новый оффер. Проверьте свою доску`);
                 }else{
-                    await client.channels.cache.get(BIZDEV_CHAN_ID).send(`[Trello] Добавлен новый оффер. Проверьте свою доску`);
+                    await client.channels.cache.get(BIZDEV_PUSH_CHAN_ID).send(`[Trello] Добавлен новый оффер. Проверьте свою доску`);
                 }
             });
         });
