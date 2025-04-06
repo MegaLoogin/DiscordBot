dd28621ad78418ce24079cb309b78507e85c7125
MegaLoogin <veh23sab@yandex.ru>
2025-04-07 01:34:41 +0300

    Update getsummary.js

--- backend/commands/main/getsummary.js
+++ backend/commands/main/getsummary.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { getMeetingTranscript } = require('../../utils/meetingService');
-const { splitMessage } = require('../../index');
+const { splitMessage } = require('../..');
 
 module.exports = {
     data: new SlashCommandBuilder()
